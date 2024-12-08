5ebf97335a530a11e813a30dea0149b4e3a62b96
yoyo <hsjejxys@gmail.com>
2024-12-08 23:14:55 +0900

    first-commit

---
+++ src/main/java/com/example/game_lobby_server/GameLobbyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_lobby_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+public class GameLobbyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLobbyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/game_lobby_server/GameLobbyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game_lobby_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLobbyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
