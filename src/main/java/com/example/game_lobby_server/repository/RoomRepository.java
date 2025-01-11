56bba9563250db5a898471ed9d12fc2105c590f1
yoyo <hsjejxys@gmail.com>
2025-01-11 16:32:20 +0900

    RoomRepositoryを作成

--- src/main/java/com/example/game_lobby_server/repository/RoomRepository.java
+++ src/main/java/com/example/game_lobby_server/repository/RoomRepository.java
@@ -1,2 +1,9 @@
-package com.example.game_lobby_server.repository;public interface RoomRepository {
+package com.example.game_lobby_server.repository;
+
+import com.example.game_lobby_server.entity.RoomEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<RoomEntity, Integer> {
 }
